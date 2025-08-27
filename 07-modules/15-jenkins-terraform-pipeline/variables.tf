bb53c42107ac7a200923f5129be8c34a8ba76ce2
oren <orensalmi1984@gmail.com>
2025-08-27 19:29:36 +0300

    Clarify aws_region variable with additional comment

--- 07-modules/15-jenkins-terraform-pipeline/variables.tf
+++ 07-modules/15-jenkins-terraform-pipeline/variables.tf
@@ -1,5 +1,7 @@
+# Define the AWS region variable
+# This allows flexibility to deploy the S3 bucket in different regions
 variable "aws_region" {
   description = "AWS region for the S3 bucket"
   type        = string
-  default     = "us-east-1"
+  default     = "us-east-1"  # default region is US East (N. Virginia)
 }

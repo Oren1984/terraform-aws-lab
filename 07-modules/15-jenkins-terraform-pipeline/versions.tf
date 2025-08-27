a943733efcd2719c0f90e5792bdcf981956078b9
oren <orensalmi1984@gmail.com>
2025-08-27 19:30:04 +0300

    Update versions.tf

--- 07-modules/15-jenkins-terraform-pipeline/versions.tf
+++ 07-modules/15-jenkins-terraform-pipeline/versions.tf
@@ -1,10 +1,17 @@
+# Terraform settings and provider requirements
 terraform {
+  # Minimum Terraform version required
   required_version = ">= 1.5.0"
+
+  # Providers used in this project
   required_providers {
+    # AWS provider to manage AWS resources
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+
+    # Random provider to generate random values (e.g., suffix for S3 bucket)
     random = {
       source  = "hashicorp/random"
       version = "~> 3.6"
@@ -12,6 +19,8 @@ terraform {
   }
 }
 
+# AWS provider configuration
 provider "aws" {
+  # Region is passed as a variable (default = us-east-1, see variables.tf)
   region = var.aws_region
 }
